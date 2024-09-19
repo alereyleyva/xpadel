e6897990305aa8b29571908cedff8463d1070bd6
Alejandro Rey Leyva <alereyleyva@gmail.com>
2024-09-20 00:07:20 +0200

    Backend | Add initial project structure

---
+++ backend/src/Controller/HealthController.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HealthController extends AbstractController
+{
+    #[Route('/health', name: 'app_health')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'XPadel API is up and running',
+        ]);
+    }
+}
