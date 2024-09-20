a0ba6ce2412192388fcf1a4b41621919fe6a9eda
Alejandro Rey Leyva <alereyleyva@gmail.com>
2024-09-20 02:03:30 +0200

    Backend | Integrate JWT creation in existing credential user login

--- backend/src/Controller/LoginController.php
+++ backend/src/Controller/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,6 +12,8 @@ use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class LoginController extends AbstractController
 {
+    public function __construct(private JWTTokenManagerInterface $tokenManager) {}
+
     #[Route('/api/login', name: 'app_login')]
     public function index(#[CurrentUser] ?User $user): JsonResponse
     {
@@ -20,9 +23,8 @@ class LoginController extends AbstractController
             ], Response::HTTP_UNAUTHORIZED);
         }
 
-
         return $this->json([
-            'user' => $user->getUserIdentifier(),
+            'accessToken' => $this->tokenManager->create($user),
         ]);
     }
 }
