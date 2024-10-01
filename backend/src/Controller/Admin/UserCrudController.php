7b59c40da04e0b0cb4ebb8d7402bff9ced99abaf
Alejandro Rey Leyva <alereyleyva@gmail.com>
2024-10-02 01:18:55 +0200

    Backend | Create initial User dashboard

---
+++ backend/src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserCrudController extends AbstractCrudController
+{
+    public function __construct(private readonly UserPasswordHasherInterface $passwordHasher) {}
+
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setSearchFields(['email'])
+            ->setDefaultSort(['createdAt' => 'DESC'])
+        ;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            EmailField::new('email'),
+            TextField::new('password')
+                ->setFormType(PasswordType::class)
+                ->onlyWhenCreating(),
+            ArrayField::new('roles')->setSortable(false),
+            DateTimeField::new('createdAt')->onlyOnIndex(),
+        ];
+    }
+
+    public function createEntity(string $entityFqcn): User
+    {
+        return new User('');
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $userRepository = $entityManager->getRepository(User::class);
+
+        $user = $userRepository->findOneBy(['email' => $entityInstance->getEmail()]);
+
+        if (null === $user) {
+            $hashedPassword = $this->passwordHasher->hashPassword($entityInstance, $entityInstance->getPassword());
+            $entityInstance->setPassword($hashedPassword);
+        }
+
+        parent::persistEntity($entityManager, $entityInstance);
+    }
+}
