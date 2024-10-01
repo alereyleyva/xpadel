38dcd2dcf34d9fc23e3f24e53c68a475753a86ec
Alejandro Rey Leyva <alereyleyva@gmail.com>
2024-10-01 21:45:33 +0200

    Frontend | Add checkbox for future terms and conditions approval

--- frontend/app/routes/_auth.register.tsx
+++ frontend/app/routes/_auth.register.tsx
@@ -1,11 +1,9 @@
 import { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
-import { Button, Input, Tooltip } from "@nextui-org/react";
-import {
-  useActionData,
-  useNavigation,
-} from "@remix-run/react";
+import { Button, Checkbox, Input, Tooltip } from "@nextui-org/react";
+import { useActionData, useNavigation } from "@remix-run/react";
 import { action as authAction } from "~/routes/_auth";
 import { FormValidationErrors } from "~/services/form-validation";
+import { useState } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -19,6 +17,7 @@ export async function action(args: ActionFunctionArgs) {
 }
 
 export default function RegisterPage() {
+  const [termsAndConditions, setTermsAndConditions] = useState<boolean>(false);
   const actionData = useActionData<FormValidationErrors>();
   const navigation = useNavigation();
 
@@ -53,13 +52,25 @@ export default function RegisterPage() {
         errorMessage={fieldErrors.repeatedPassword}
       />
 
+      <Checkbox
+        checked={termsAndConditions}
+        onChange={(event) => setTermsAndConditions(event.target.checked)}
+      >
+        He leído y acepto los términos y condiciones de uso
+      </Checkbox>
+
       <Tooltip
         content={formErrors[0]}
         placement="bottom"
         isOpen={Object.keys(fieldErrors).length === 0 && formErrors.length > 0}
         color="danger"
       >
-        <Button color="primary" type="submit" isLoading={isSubmitting}>
+        <Button
+          color="primary"
+          type="submit"
+          isLoading={isSubmitting}
+          isDisabled={!termsAndConditions}
+        >
           Iniciar sesión
         </Button>
       </Tooltip>
