908f2b4d92a22128ef87a1da57146ee175ebb398
Alejandro Rey Leyva <alereyleyva@gmail.com>
2024-10-03 05:58:50 +0200

    Frontend | Create initial layout with profile page

--- frontend/app/routes/_home.profile.tsx
+++ frontend/app/routes/_home.profile.tsx
@@ -1,8 +1,7 @@
 import { LoaderFunctionArgs } from "@remix-run/node";
 import { authenticator } from "~/services/auth.server";
-import { Form, useLoaderData } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 import { UserSession } from "~/types/definitions";
-import { Button } from "@nextui-org/react";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   return await authenticator.isAuthenticated(request, {
@@ -14,12 +13,9 @@ export default function Profile() {
   const user = useLoaderData<UserSession>();
 
   return (
-    <div className="flex h-screen flex-col items-center justify-center space-y-5">
+    <div className="mt-10 flex flex-col items-center justify-center">
       <h1 className="text-3xl">Profile Page</h1>
       <p>Bienvenido/a {user.email}</p>
-      <Form method="post" action="/logout">
-        <Button type="submit">Cerrar sesión</Button>
-      </Form>
     </div>
   );
 }
---
+++ frontend/app/routes/_home.tsx
@@ -0,0 +1,84 @@
+import { LoaderFunctionArgs } from "@remix-run/node";
+import { authenticator } from "~/services/auth.server";
+import icon from "~/images/xpadel.png";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  Link,
+  DropdownItem,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+  Avatar,
+} from "@nextui-org/react";
+import {
+  Outlet,
+  useLoaderData,
+  useNavigate,
+  useSubmit,
+} from "@remix-run/react";
+import { UserSession } from "~/types/definitions";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  return await authenticator.isAuthenticated(request, {
+    failureRedirect: "/login",
+  });
+}
+
+export default function Home() {
+  const { email } = useLoaderData<UserSession>();
+  const navigate = useNavigate();
+  const submit = useSubmit();
+
+  function handleLogout() {
+    submit(null, { action: "/logout", method: "post" });
+  }
+
+  return (
+    <div className="min-h-screen">
+      <Navbar isBordered isBlurred maxWidth="lg">
+        <NavbarBrand>
+          <Link href="/">
+            <img src={icon} alt="xpadel-icon" className="w-7" />
+          </Link>
+        </NavbarBrand>
+
+        <NavbarContent as="div" justify="end">
+          <Dropdown placement="bottom-end">
+            <DropdownTrigger>
+              <Avatar
+                as="button"
+                className="transition-transform"
+                color="default"
+                name={email}
+                size="md"
+              />
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Profile Actions" variant="flat">
+              <DropdownItem key="profile" className="h-8" textValue="profile">
+                <p className="font-semibold">{email}</p>
+              </DropdownItem>
+              <DropdownItem
+                key="settings"
+                textValue="settings"
+                onClick={() => navigate("/profile")}
+              >
+                Mi perfil de jugador
+              </DropdownItem>
+              <DropdownItem
+                key="logout"
+                color="danger"
+                textValue="logout"
+                onClick={handleLogout}
+              >
+                Cerrar sesión
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </NavbarContent>
+      </Navbar>
+      <Outlet />
+    </div>
+  );
+}
