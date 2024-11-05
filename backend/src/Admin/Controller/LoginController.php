e616ad84b62a1ef5e5e9fad67b1ee255c107e560
Alejandro Rey Leyva <alereyleyva@gmail.com>
2024-11-05 05:58:23 +0100

    Backend | Admin | Move all related files to independent directory

--- backend/src/Admin/Controller/LoginController.php
+++ backend/src/Admin/Controller/LoginController.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace App\Controller\Admin;
+namespace App\Admin\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
--- backend/src/Admin/Controller/UserProfileCrudController.php
+++ backend/src/Admin/Controller/UserProfileCrudController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\Admin;
+namespace App\Admin\Controller;
 
 use App\Entity\UserProfile;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
